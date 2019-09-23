729340314e07a9538e302e9b9f1e98aee2cc69f0
Tony Stark <swzyocowboy@icloud.com>
2019-09-23 18:49:05 +0800

    功能: :gear: 更新 404 页面

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,9 +11,9 @@ class NotFoundPage extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <SEO title="404: 无法找到该页面" />
+        <h1>我错了</h1>
+        <p>居然把你需要的页面弄丢了...</p>
       </Layout>
     );
   }
